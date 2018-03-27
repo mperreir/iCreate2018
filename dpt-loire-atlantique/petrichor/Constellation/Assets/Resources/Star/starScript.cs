4f48a3a484bc015d538dfd57aa1d07002a1df3fd
Kilian Riou <kilian@redheness.net>
2018-03-27 14:00:48 +0200

    Ajout d'assets
    Scene
    Etoile

---
+++ dpt-loire-atlantique/petrichor/Constellation/Assets/Resources/Star/starScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starScript : MonoBehaviour {
+
+	public int num = 0;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		this.transform.position = this.transform.position + new Vector3(Mathf.Sin(Time.time + (num / 5F )), 0, 0);
+	}
+}
