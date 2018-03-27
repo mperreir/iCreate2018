24274ec9bf731325fff7481f7a42a48738aed2de
Kilian Riou <kilian@redheness.net>
2018-03-27 17:39:59 +0200

    Demo camera

---
+++ dpt-loire-atlantique/petrichor/Constellation/Assets/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = this.transform.position + new Vector3(0, 0, Time.time / 20F);
+	}
+}
