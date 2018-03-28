6b113e8edd2042036288d16677eadb2dc18b5a96
Andréas PASTOR <andreas.pastor@etu.univ-nantes.fr>
2018-03-28 08:45:39 +0200

    Add bases of the project

---
+++ musee-d-art/club-sandwitch/client/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SoundPlayerPage from './Pages/SoundPlayerPage';
+import TabletPage from './Pages/TabletPage';
+import LandingPage from './Pages/LandingPage';
+
+import 'whatwg-fetch';
+import './App.css';
+
+class App extends Component {
+
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={LandingPage}/>
+        <Route path='/tablet' component={TabletPage}/>
+        <Route path='/soundplayer' component={SoundPlayerPage}/>
+      </Switch>
+    );
+  }
+}
+
+export default App;
---
+++ musee-d-art/club-sandwitch/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var app = express();
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+io.on('connection', function(socket) {
+    console.log(socket.id);
+    socket.on('update', () => {
+        console.log('emiting');
+        io.emit('update');
+    });
+    socket.on('connect-tablet', (msg) => {
+      console.log('tablet connected! ' + msg);
+    });
+    socket.on('connect-sound-player', (msg) => {
+        console.log('soundPlayer connected! ' + msg);
+    });
+});
+
+server.listen(process.env.PORT || 5000, function() {
+    console.log('listening on port');
+});
+
