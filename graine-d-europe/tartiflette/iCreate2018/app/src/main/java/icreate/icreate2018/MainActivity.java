54e5fe38d8af526770359e821344a27a3becc0d5
Brewal HENAFF <brewal.henaff@etu.univ-nantes.fr>
2018-03-28 11:38:41 +0200

    Changement visuel + accelerometre

--- graine-d-europe/tartiflette/iCreate2018/app/src/main/java/icreate/icreate2018/MainActivity.java
+++ graine-d-europe/tartiflette/iCreate2018/app/src/main/java/icreate/icreate2018/MainActivity.java
@@ -2,6 +2,7 @@ package icreate.icreate2018;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -17,23 +18,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Button> buttonList = new ArrayList<>();
-        buttonList.add((Button)findViewById(R.id.button_1));
-        buttonList.add((Button)findViewById(R.id.button_2));
-        buttonList.add((Button)findViewById(R.id.button_3));
+        ConstraintLayout ml = findViewById(R.id.mylayout);
 
+        ml.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(MainActivity.this, ThemeActivity.class);
 
-        for (final Button bt : buttonList) {
-            bt.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                    Intent intent1 = new Intent(MainActivity.this, ThemeActivity.class);
-                    intent1.putExtra(EXTRA_THEME_NAME, bt.getText());
-
-                    startActivity(intent1);
-                }
-            });
-        }
+                startActivity(intent1);
+            }
+        });
     }
 }
