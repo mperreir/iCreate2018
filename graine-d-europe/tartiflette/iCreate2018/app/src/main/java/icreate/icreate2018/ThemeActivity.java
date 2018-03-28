656ca2fc561b96cbb7fbc18b5878ac6364eaefe0
solangedaguise <daguisesolange@gmail.com>
2018-03-28 16:30:02 +0200

    affichage question

--- graine-d-europe/tartiflette/iCreate2018/app/src/main/java/icreate/icreate2018/ThemeActivity.java
+++ graine-d-europe/tartiflette/iCreate2018/app/src/main/java/icreate/icreate2018/ThemeActivity.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
+import android.os.SystemClock;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +19,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ThemeActivity extends AppCompatActivity implements SensorEventListener {
+    final static String Q_CITOYENNETE = "Qu'est-ce-que la citoyenneté Européenne ?";
+    final static String Q_ETRE_CITOYEN = "Qu'est-ce-que ça nous apporte d'être citoyen Européen ?";
+    final static String Q_ROLE_CITOYEN = "Quel est notre rôle en tant que citoyen Européen ?";
+
     // Déclaration de l'attribut en tant qu'attribut de l'activité
     // Le sensor manager (gestionnaire de capteurs)
     SensorManager sensorManager;
@@ -33,6 +38,10 @@ public class ThemeActivity extends AppCompatActivity implements SensorEventListe
     TextView txt_x;
     TextView txt_y;
     TextView txt_z;
+
+    TextView q_1;
+    TextView q_2;
+    TextView q_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,22 +59,32 @@ public class ThemeActivity extends AppCompatActivity implements SensorEventListe
         txt_y = findViewById(R.id.y);
         txt_z = findViewById(R.id.z);
 
+        q_1 = findViewById(R.id.q_1);
+        q_2 = findViewById(R.id.q_2);
+        q_3 = findViewById(R.id.q_3);
+
+
+        //Gestion de la lecture audio des question
         Button play_bt = findViewById(R.id.play);
         mediaPlayer_1 = MediaPlayer.create(this, R.raw.citoyennete_europeene);
         mediaPlayer_2 = MediaPlayer.create(this, R.raw.etre_citoyen_europeen);
         mediaPlayer_3 = MediaPlayer.create(this, R.raw.role_citoyen_europeen);
 
+        //Ordre dans lequel les questions sont lues
         mediaPlayer_1.setNextMediaPlayer(mediaPlayer_2);
         mediaPlayer_2.setNextMediaPlayer(mediaPlayer_3);
 
+        //Lecture des question associées à un bouton
         play_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mediaPlayer_1.start();
+
                 Toast.makeText(getApplicationContext(), "Coucou ça fonctionne", Toast.LENGTH_SHORT).show();
 
             }
         });
+
     }
 
     @Override
@@ -98,6 +117,28 @@ public class ThemeActivity extends AppCompatActivity implements SensorEventListe
             txt_x.setText(""+event.values[0]);
             txt_y.setText(""+event.values[1]);
             txt_z.setText(""+event.values[2]);
+
+            //Gestion direction du balancier
+            //TRAITEMENT DES QUESTIONS
+            if(event.values[0] < -3 || event.values[1] > 2 || event.values[1] > -2){
+                if(event.values[0] < -3 && event.values[1] < 1 && event.values[1] > -1){
+                    q_1.setText(""+Q_CITOYENNETE);
+                    q_2.setText("");
+                    q_3.setText("");
+                }
+                else if (event.values[1] < -1){
+                    q_1.setText("");
+                    q_2.setText(""+Q_ROLE_CITOYEN);
+                    q_3.setText("");
+
+                }
+                else if (event.values[1] > 1){
+                    q_1.setText("");
+                    q_2.setText("");
+                    q_3.setText(""+Q_ETRE_CITOYEN);
+                }
+            }
+
         }
     }
 }
