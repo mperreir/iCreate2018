ba26a96b7982b57b444e7277964e1187d0cc9a6c
Kilian Riou <kilian@redheness.net>
2018-03-28 17:46:14 +0200

    Ajouts de fonctionnalités
    Galaxies
    Structure pour selection

---
+++ dpt-loire-atlantique/petrichor/Constellation/Assets/Resources/Galaxy/GalaxyScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GalaxyScript : MonoBehaviour {
+
+	public string galaxyName = "";
+	public string galaxyTag = "";
+	public float radius = 5;
+	public float speed;
+	public Vector3 rotator = new Vector3(0, 1, 2);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Rotate(rotator, this.speed*Time.deltaTime);
+	}
+}
--- dpt-loire-atlantique/petrichor/Constellation/Assets/StarManager.cs
+++ dpt-loire-atlantique/petrichor/Constellation/Assets/StarManager.cs
@@ -5,26 +5,37 @@ using System.IO;
 
 public class StarManager : MonoBehaviour {
 
-	private List<GameObject> stars;
+	private List<StarScript> stars;
+	private List<GalaxyScript> galaxies;
+
+	public string criteria = "all";
+	public float galaxyDensity = 200;
+	public float galaxySpeed = 22;
 	public int initialDisplayLimit = -1;
+	public float minx = -10;
+	public float maxx = 10;
+	public float miny = -10;
+	public float maxy = 10;
+	public float minz = 0;
+	public float maxz = 20;
 
 	// Use this for initialization
 	void Start ()
 	{
-		stars = new List<GameObject>();
-		//GameObject prefab = Resources.Load("Star/StarPrefab", typeof(GameObject)) as GameObject;
-		GameObject prefab = Resources.Load("StarSprite/StarSpritePrefab", typeof(GameObject)) as GameObject;
-		GameObject star;
+		stars = new List<StarScript>();
+		galaxies = new List<GalaxyScript>();
+		StarScript prefab = (Resources.Load("Star/StarPrefab", typeof(GameObject)) as GameObject).GetComponent<StarScript>();
+		StarScript star;
 		StreamReader dataFile = new StreamReader("Assets/Resources/soldats_memorial_virtuel.csv");
 		string rawLine = dataFile.ReadLine();
 		string[] dataLine;
-		string residence = null;
-		string grade = null;
-		string profession = null;
-		string naissance = null;
-		string volontaire = null;
-		int age = 0;
-		int num = 0;
+		string residence;
+		string grade;
+		string profession;
+		string naissance;
+		string volontaire;
+		int age;
+		int num ;
 		int width = 0;
 		bool firstLine = true;
 		int i = 0;
@@ -38,6 +49,14 @@ public class StarManager : MonoBehaviour {
 			}
 			else if (dataLine.Length == width)
 			{
+				residence = null;
+				grade = null;
+				profession = null;
+				naissance = null;
+				volontaire = null;
+				age = 0;
+				num = 0;
+				width = 0;
 				System.Int32.TryParse(dataLine[0], out num);
 				System.Int32.TryParse(dataLine[11], out age);
 				if (dataLine[13] != "")
@@ -70,10 +89,10 @@ public class StarManager : MonoBehaviour {
 				star.GetComponent<StarScript>().profession = profession;
 				star.GetComponent<StarScript>().naissance = naissance;
 				star.GetComponent<StarScript>().volontaire = volontaire;
-				star.transform.position = new Vector3(10-Random.value*20, 5-Random.value*10, Random.value*20);
+				star.transform.position = new Vector3(this.minx - Random.value * (this.maxx-minx), this.miny - Random.value * (this.maxy - miny), this.minz - Random.value * (this.maxz - minz));
 				if (i > this.initialDisplayLimit && this.initialDisplayLimit != -1)
 				{
-					star.SetActive(false);
+					star.gameObject.SetActive(false);
 				}
 				this.stars.Add(star);
 				rawLine = dataFile.ReadLine();
@@ -81,8 +100,57 @@ public class StarManager : MonoBehaviour {
 			}
 		}
 		dataFile.Close();
+		this.ChangeCriteria(this.criteria);
 	}
-	
+
+	void ChangeCriteria(string criteria)
+	{
+		int i;
+		List<string> tags = new List<string>();
+		this.criteria = criteria;
+		for (i = 0; i < this.galaxies.Count; i++)
+		{
+			Destroy(this.galaxies[i]);
+		}
+		this.galaxies.Clear();
+		if (criteria == "all")
+		{
+			this.galaxies.Add(CreateGalaxy(this.stars.Count, "all", "", this.galaxyDensity));
+			for (i = 0; i < this.stars.Count; i++)
+			{
+				this.stars[i].MoveTo(this.galaxies[0]);
+			}
+		}
+	}
+
+	GalaxyScript FindGalaxy(string tag)
+	{
+		int i;
+		for (i = 0; i < galaxies.Count; i++)
+		{
+			if (galaxies[i].tag == tag)
+			{
+				return galaxies[i];
+			}
+		}
+		return null;
+	}
+
+	GalaxyScript CreateGalaxy(int num, string tag, string name, float density)
+	{
+		GalaxyScript galaxy = Instantiate((Resources.Load("Galaxy/GalaxyPrefab", typeof(GameObject)) as GameObject).GetComponent<GalaxyScript>());
+		galaxy.name = tag;
+		galaxy.galaxyTag = tag;
+		galaxy.galaxyName = name;
+		galaxy.radius = Mathf.Sqrt(num) / density;
+		galaxy.transform.position = new Vector3(this.minx - Random.value * (this.maxx - minx), this.miny - Random.value * (this.maxy - miny), this.minz - Random.value * (this.maxz - minz));
+		galaxy.rotator = new Vector3(1 - Random.value* 2, 1 - Random.value* 2, 1 - Random.value* 2);
+		galaxy.name = tag;
+		galaxy.transform.SetParent(this.transform);
+		galaxy.speed = Random.value * this.galaxySpeed;
+		this.galaxies.Add(galaxy);
+		return galaxy;
+	}
 	// Update is called once per frame
 	void Update ()
 	{
