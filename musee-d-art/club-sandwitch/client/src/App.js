0da56223986bf9cdfd348bb923f5355f58511cf5
Andréas PASTOR <andreas.pastor@etu.univ-nantes.fr>
2018-03-28 08:56:21 +0200

    Add folder for working in branches

--- musee-d-art/club-sandwitch/client/src/App.js
+++ musee-d-art/club-sandwitch/client/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import SoundPlayerPage from './Pages/SoundPlayerPage';
 import TabletPage from './Pages/TabletPage';
 import LandingPage from './Pages/LandingPage';
+import SketchPage from './Pages/SketchPage';
 
 import 'whatwg-fetch';
 import './App.css';
@@ -33,6 +34,7 @@ class App extends Component {
         <Route exact path='/' component={LandingPage}/>
         <Route path='/tablet' component={TabletPage}/>
         <Route path='/soundplayer' component={SoundPlayerPage}/>
+        <Route path='/sketch' component={SketchPage}/>
       </Switch>
     );
   }
