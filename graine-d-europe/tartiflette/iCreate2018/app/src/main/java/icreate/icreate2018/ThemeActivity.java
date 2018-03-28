cfc96bf2697d225fb9e525400848421b163e5082
Brewal HENAFF <brewal.henaff@etu.univ-nantes.fr>
2018-03-28 14:25:50 +0200

    mp3 in res/raw + lecture des fichiers audio

--- graine-d-europe/tartiflette/iCreate2018/app/src/main/java/icreate/icreate2018/ThemeActivity.java
+++ graine-d-europe/tartiflette/iCreate2018/app/src/main/java/icreate/icreate2018/ThemeActivity.java
@@ -5,17 +5,27 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ThemeActivity extends AppCompatActivity implements SensorEventListener {
     // Déclaration de l'attribut en tant qu'attribut de l'activité
     // Le sensor manager (gestionnaire de capteurs)
     SensorManager sensorManager;
+
+    MediaPlayer mediaPlayer_1;
+    MediaPlayer mediaPlayer_2;
+    MediaPlayer mediaPlayer_3;
+
 
     // L'accéléromètre
     Sensor accelerometer;
@@ -39,6 +49,23 @@ public class ThemeActivity extends AppCompatActivity implements SensorEventListe
         txt_x = findViewById(R.id.x);
         txt_y = findViewById(R.id.y);
         txt_z = findViewById(R.id.z);
+
+        Button play_bt = findViewById(R.id.play);
+        mediaPlayer_1 = MediaPlayer.create(this, R.raw.citoyennete_europeene);
+        mediaPlayer_2 = MediaPlayer.create(this, R.raw.etre_citoyen_europeen);
+        mediaPlayer_3 = MediaPlayer.create(this, R.raw.role_citoyen_europeen);
+
+        mediaPlayer_1.setNextMediaPlayer(mediaPlayer_2);
+        mediaPlayer_2.setNextMediaPlayer(mediaPlayer_3);
+
+        play_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer_1.start();
+                Toast.makeText(getApplicationContext(), "Coucou ça fonctionne", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 
     @Override
