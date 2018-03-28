ca54a3f5e4f07f7c78f7704cedc14cfb38d94eea
Félix Arnoult <felix.arnoult@etu.univ-nantes.fr>
2018-03-28 17:10:03 +0200

    Souns player app

--- musee-d-art/club-sandwitch/client/src/Pages/SoundPlayerPage.js
+++ musee-d-art/club-sandwitch/client/src/Pages/SoundPlayerPage.js
@@ -1,36 +1,32 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import io from 'socket.io-client';
 import ReactAudioPlayer from 'react-audio-player';
 import P5Wrapper from 'react-p5-wrapper';
-
+import audio from '../sketches/audio';
 import sketch from '../sketches/sketch';
 
 class SoundPlayerPage extends Component {
 
-    constructor() {
-        super();
-        this.socket = io();//SocketIOClient('http://localhost:5000');
-        this.socket.on('connect', () => {
-            console.log(this.socket.id); // 'G5p5...'
-            const idSocket = this.socket.id;
-            this.socket.emit('connect-tablet', idSocket);
-        });
-        this.socket.on('update', () => this.setState({name: 'Felix'}));
+  constructor() {
+    super();
+    this.state = {
+      nomFct: "",
+      arg: "/Grenouille.mp3"
     }
+    this.socket = io(); //SocketIOClient('http://localhost:5000');
+    this.socket.on('connect', () => {
+      console.log(this.socket.id); // 'G5p5...'
+      const idSocket = this.socket.id;
+      this.socket.emit('connect-tablet', idSocket);
+    });
+    this.socket.on('update', () => this.setState({nomFct: "play"}));
+    this.socket.on('stopSong', () => this.setState({nomFct: "stop"}));
+    this.socket.on('pauseSong', () => this.setState({nomFct: "pause"}));
+  }
 
-    render() {
-        return (
-            <div>
-                <ReactAudioPlayer
-                    src="CORNE .wav"
-                    autoPlay
-                    loop
-                />
-                <P5Wrapper sketch={sketch} />
-                <p>Sound Player Page</p>
-            </div>
-        );
-    }
+  render() {
+    return (<P5Wrapper sketch={audio} nomFct={this.state.nomFct} arg={this.state.arg}/>);
+  }
 }
 
 export default SoundPlayerPage;
