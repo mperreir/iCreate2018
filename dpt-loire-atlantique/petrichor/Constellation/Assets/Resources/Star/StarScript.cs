217de60df6ad9501cc14e184a992ff360e79436e
Kilian Riou <kilian@redheness.net>
2018-03-29 20:30:54 +0200

    Modifications
    Modification de la scène
    Ajout du controleur
    Amelioration des galaxies

--- dpt-loire-atlantique/petrichor/Constellation/Assets/Resources/Star/StarScript.cs
+++ dpt-loire-atlantique/petrichor/Constellation/Assets/Resources/Star/StarScript.cs
@@ -6,7 +6,7 @@ public class StarScript : MonoBehaviour {
 
 	public int num = 0;
 	public int age = 0;
-	public float galaxyDistance = 0;
+	public Vector3 galaxyPosition = new Vector3();
 	public float joinSpeed = 2;
 	public string residence = null;
 	public string grade = null;
@@ -29,16 +29,17 @@ public class StarScript : MonoBehaviour {
 		{
 			this.transform.position = this.transform.position + new Vector3(Time.deltaTime * Mathf.Sin(Time.time * 5F + (this.num / 50F)), 0, 0);
 		}
-		else if (!attached)
+		else if (!this.attached)
 		{
-			if (Vector3.Distance(this.galaxy.transform.position, this.transform.position) < this.galaxyDistance)
+			if (Vector3.Distance(this.transform.position, this.galaxy.transform.position - this.galaxyPosition) < Time.deltaTime * this.joinSpeed)
 			{
 				this.attached = true;
 				this.transform.SetParent(this.galaxy.transform);
+				this.transform.transform.position = this.galaxy.transform.position - this.galaxyPosition;
 			}
 			else
 			{
-				this.transform.transform.position = Vector3.MoveTowards(this.transform.position, this.galaxy.transform.position, this.joinSpeed * Time.deltaTime);
+				this.transform.transform.position = Vector3.MoveTowards(this.transform.position, this.galaxy.transform.position - this.galaxyPosition, this.joinSpeed * Time.deltaTime);
 			}
 		}
 	}
@@ -46,7 +47,7 @@ public class StarScript : MonoBehaviour {
 	public void MoveTo(GalaxyScript galaxy)
 	{
 		this.galaxy = galaxy;
-		this.galaxyDistance = Random.value * galaxy.radius;
+		this.galaxyPosition = Random.insideUnitSphere*galaxy.radius;
 		this.attached = false;
 	}
 
---
+++ dpt-loire-atlantique/petrichor/Constellation/Assets/Scripts/Controler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controler : MonoBehaviour {
+
+	public CameraScript cam;
+	public StarManager SM;
+	public int StarsPerKey = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("key"))
+		{
+			this.KeyAction();
+		}
+		if (Input.GetButtonDown("tab"))
+		{
+			this.TabAction();
+		}
+		if (Input.GetButtonDown("ding"))
+		{
+			this.DingAction();
+		}
+		if (Input.GetButtonDown("return"))
+		{
+			this.ReturnAction();
+		}
+	}
+
+	public void KeyAction()
+	{
+		this.SM.SpawnStars(this.StarsPerKey);
+	}
+
+	public void TabAction()
+	{
+		this.SM.NextCriteria();
+	}
+
+	public void DingAction()
+	{
+		this.SM.Scint();
+	}
+
+	public void ReturnAction()
+	{
+		this.SM.ResetStars();
+	}
+}
