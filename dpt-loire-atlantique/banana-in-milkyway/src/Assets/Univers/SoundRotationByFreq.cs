6ec4cfe8425796c21312e7eb6e97ae77c1bc080b
Fabien HUDIER <fabien.hudier1@etu.univ-nantes.fr>
2018-03-29 18:22:18 +0200

    C bon github est content avec son "file size restrict" !!!

---
+++ dpt-loire-atlantique/banana-in-milkyway/src/Assets/Univers/SoundRotationByFreq.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRotation : MonoBehaviour {
+
+	public float radius, frequency;
+	public int beginAngle;
+	
+	private float angle, speed;
+	private AudioSource audio;
+	private int counter;
+		
+	// Use this for initialization
+	void Start () {
+		angle = beginAngle;
+		speed = 4f * radius;
+		audio = gameObject.GetComponent<AudioSource>();
+		counter = 1;
+	}
+
+	// Update is called once per frame
+	private void Update () {
+		angle = (angle + (Time.deltaTime * speed)) % 360f;
+		float radian = angle * 0.0174532925f;
+		transform.localPosition = new Vector3(radius * Mathf.Cos(radian), transform.localPosition.y, radius * Mathf.Sin(radian));
+		
+		counter++;
+		if(counter > 100){
+			counter = 0;
+			if(Random.value < frequency && !audio.isPlaying){
+				Debug.Log("Paly !");
+				audio.Play();
+			}
+		}
+	}
+}
---
+++ dpt-loire-atlantique/banana-in-milkyway/src/Assets/Univers/SoundRotationBySec.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundRotation : MonoBehaviour {
+
+	public float radius, seconds;
+	public int beginAngle;
+	
+	private float angle, speed;
+	private AudioSource audio;
+		
+	// Use this for initialization
+	void Start () {
+		angle = beginAngle;
+		speed = 4f * radius;
+		audio = gameObject.GetComponent<AudioSource>();
+		StartCoroutine(PlayDelayedClip());
+	}
+	 
+	IEnumerator PlayDelayedClip()
+	{
+		yield return new WaitForSeconds(seconds);
+		if(!audio.isPlaying)
+			audio.Play();
+	}
+
+	// Update is called once per frame
+	private void Update () {
+		angle = (angle + (Time.deltaTime * speed)) % 360f;
+		float radian = angle * 0.0174532925f;
+		transform.localPosition = new Vector3(radius * Mathf.Cos(radian), transform.localPosition.y, radius * Mathf.Sin(radian));
+	}
+	
+	
+}
