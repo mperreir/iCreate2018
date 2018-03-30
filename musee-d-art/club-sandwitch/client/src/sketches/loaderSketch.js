74739c8fae9f7d6d0d85421376f602b6c679ca0e
FelixArnoult <felix.arnoult@etu.univ-nantes.fr>
2018-03-30 13:32:00 +0200

    Add play pause stop function

--- musee-d-art/club-sandwitch/client/src/sketches/loaderSketch.js
+++ musee-d-art/club-sandwitch/client/src/sketches/loaderSketch.js
@@ -6,33 +6,42 @@ let hTab = 1024;
 let hRect = 100; //Hauteur de la barre de chargement
 let wRect = 0;
 
-let play = true;
+let play = false;
 let time;
 
 export default function sketch(p) {
   p.setup = function() {
     p.createCanvas(wTab, hRect);
     p.noFill();
-    p.rect(0, 0, wTab-1, hRect-1);
+    p.rect(0, 0, wTab - 1, hRect - 1);
     p.fill(0);
-    p.rect(marge, marge, wRect, hRect - marge*2);
+    p.rect(marge, marge, wRect, hRect - marge * 2);
     time = p.millis();
   };
 
   p.draw = function() {
     //console.log(time);
-    if (p.millis() - time > 50) {
+    if ((p.millis() - time > 50) && play) {
       time = p.millis();
-      //p.background(0);
       wRect++;
       console.log(wRect);
-      p.rect(marge, marge, wRect, hRect - marge*2);
+      p.rect(marge, marge, wRect, hRect - marge * 2);
 
     }
   };
 
   p.myCustomRedrawAccordingToNewPropsHandler = function(props) {
-    // this.play = props.isPlaying;
-    //scareInt.w += 40;
-  };
-}
+    switch (props.nomFct) {
+      case "play":
+        play = true;
+        break;
+      case "pause":
+        play = false;
+        break;
+      case "stop":
+        wRect = 0;
+        play = false;
+        break;
+      default:
+    };
+  }
