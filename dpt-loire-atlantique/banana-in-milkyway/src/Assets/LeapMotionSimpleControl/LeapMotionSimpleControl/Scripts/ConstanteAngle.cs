1cc59574359971816d3511b556dce5919924e55b
Drest <nicolas.chabot1@etu.univ-nantes.fr>
2018-03-30 11:48:55 +0200

    son spatial + mouvement

---
+++ dpt-loire-atlantique/banana-in-milkyway/src/Assets/LeapMotionSimpleControl/LeapMotionSimpleControl/Scripts/ConstanteAngle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConstanteAngle
+{
+	public static int ITALIE=48;
+	public static int NANTES=0;
+	public static int ALLEMAGNE=36;
+	public static int CHINE=162;
+	public static int MEURTHE_ET_MOZEL=0;
+	public static int SERBIE=53;
+	public static int INDOCHINE=173;
+	public static int PYRENEES=0;
+	public static int CANADA=300;
+	public static int MADAGASCAR=37;
+	public static int MARTINIQUE=312;
+	public static int TCHAD=23;
+	public static int DAKAR=7;
+	public static int GRECE=60;
+	public static int ALGERIE=15;
+	public static int TURQUIE=81;
+	public static int CUISSE_LA_MONTTE_FRONT=0;
+	public static int SRI_LANTA=145;
+	public static int MEUSE_FRONT=0;
+	public static int RUSSIE=141;
+	public static int MARNE=0;
+	public static int PAS_DE_CALAIS=0;
+
+}
+
