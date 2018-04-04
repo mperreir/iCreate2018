ffbdcc3130f7c53ac65047d1fecac9f78079ad6b
gaetan <gageffroy@gmail.com>
2018-04-04 21:27:53 +0200

    Ca marche ! Premiers tests concluants, reste à affiner la reconnaissance audio

--- dpt-loire-atlantique/petrichor/SoundRecognition/SoundRecognition.py
+++ dpt-loire-atlantique/petrichor/SoundRecognition/SoundRecognition.py
@@ -1,21 +1,32 @@
 from pyAudioAnalysis import audioTrainTest as at
 import pyaudio
 import wave
+import socket
 
-def printReco(reco) :
+# Parametres UDP pour l'envoi des messages
+class UDPparam :
+	def __init__(self) :
+		self.UDP_IP = "127.0.0.1"
+		self.UDP_PORT = 5005
+		self.MESSAGE = "blanc"
+
+# Change le champ MESSAGE selon le son qui est reconnu
+def printReco(reco, params) :
 	if (reco[0] == 1.0) :
-		print("charriot")
+		params.MESSAGE = "charriot"
 	if (reco[1] == 1.0) :
-		print("clavier")
+		params.MESSAGE = "clavier"
 	if (reco[2] == 1.0) :
-		print("ding")
+		params.MESSAGE = "ding"
 	if (reco[3] == 1.0) :
-		print ("tab")
+		params.MESSAGE = "tab"
 	if (reco[4] == 1.0) :
-		print ("blanc")
+		params.MESSAGE == "blanc"
 
-at.featureAndTrain(["machineAEcrire/charriot", "machineAEcrire/clavier", "machineAEcrire/ding", "machineAEcrire/tab", "testMaison/blanc"], 1.0, 1.0, at.shortTermWindow, at.shortTermStep, "knn", "knnTypeWriterSounds", False);
+# Entrainement du programme de reconnaissance sur une base de sons pre enregistres
+#at.featureAndTrain(["machineAEcrire/charriot", "machineAEcrire/clavier", "machineAEcrire/ding", "machineAEcrire/tab", "testMaison/blanc"], 1.0, 1.0, at.shortTermWindow, at.shortTermStep, "knn", "knnTypeWriterSounds", False);
 
+# Enregistrement du micro pendant une seconde (code donne par pyAudio)
 def recordAudio() :
 	FORMAT = pyaudio.paInt16
 	CHANNELS = 2
@@ -23,27 +34,26 @@ def recordAudio() :
 	CHUNK = 1024
 	RECORD_SECONDS = 1
 	WAVE_OUTPUT_FILENAME = "records/file.wav"
-	 
-	audio = pyaudio.PyAudio()
-	 
+
+	audio = pyaudio.PyAudio();
+
 	# start Recording
 	stream = audio.open(format=FORMAT, channels=CHANNELS,
 					rate=RATE, input=True,
 					frames_per_buffer=CHUNK)
-	print ("recording...")
+	print("recording...")
 	frames = []
-	 
+
 	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
 		data = stream.read(CHUNK)
 		frames.append(data)
 	print ("finished recording")
-	 
-	 
-	# stop Recording
+
+	# stop recording
 	stream.stop_stream()
 	stream.close()
 	audio.terminate()
-	 
+
 	waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
 	waveFile.setnchannels(CHANNELS)
 	waveFile.setsampwidth(audio.get_sample_size(FORMAT))
@@ -51,13 +61,24 @@ def recordAudio() :
 	waveFile.writeframes(b''.join(frames))
 	waveFile.close()
 
-while (True) :
+# Envoi d'un paquet UDP vers le programme Unity
+def sendUDP(params) :
+	print ("UDP target IP:", params.UDP_IP)
+	print ("UDP target port : ", params.UDP_PORT)
+	print ("message : ", params.MESSAGE)
+	sock = socket.socket(socket.AF_INET, # Internet
+			     socket.SOCK_DGRAM) # UDP
+	sock.sendto(params.MESSAGE, (params.UDP_IP, params.UDP_PORT))
+
+# Programme principal
+params = UDPparam()
+params.MESSAGE = "tab"
+sendUDP(params)
+
+'''while (True) :
 	recordAudio()
-	(_, reco1, _) = at.fileClassification("testMaison/unknown/unknown-blanc.wav", "knnTypeWriterSounds", "knn")
-	#(_, reco2, _) = at.fileClassification("records/file.wav", "knnTypeWriterSounds", "knn")
-	#(_, reco3, _) = at.fileClassification("testMaison/unknown/unknown-main_short.wav", "knnTypeWriterSounds", "knn")
-	#(_, reco4, _) = at.fileClassification("testMaison/unknown/unknown-cloc_short.wav", "knnTypeWriterSounds", "knn")
-	printReco(reco1)
-#printReco(reco2)
-#printReco(reco3)
-#printReco(reco4)
+	(_,reco,_) = at.fileClassification("records/file.wav", "knnTypeWriterSounds", "knn")
+	printReco(reco, params)
+	#sendUDP()
+	print(params.MESSAGE)
+	params.MESSAGE = "blanc" # sinon on va envoyer le meme ordre en boucle'''
