1c026ba4cc88d9258119d4eb8a7563ce2cd078cf
drest <nicolas.chabot1@etu.univ-nantes.fr>
2018-04-20 12:40:00 +0200

    finish

--- dpt-loire-atlantique/banana-in-milkyway/src/Assets/LeapMotionSimpleControl/LeapMotionSimpleControl/Scripts/ConstanteAngle.cs
+++ dpt-loire-atlantique/banana-in-milkyway/src/Assets/LeapMotionSimpleControl/LeapMotionSimpleControl/Scripts/ConstanteAngle.cs
@@ -3,28 +3,28 @@ using System.Collections;
 
 public class ConstanteAngle
 {
-	public static int ITALIE=48;
+	public static int ITALIE=135;
 	public static int NANTES=0;
-	public static int ALLEMAGNE=36;
-	public static int CHINE=162;
+	public static int ALLEMAGNE=137;
+	public static int CHINE=244;
 	public static int MEURTHE_ET_MOZEL=0;
-	public static int SERBIE=53;
+	public static int SERBIE=152;
 	public static int INDOCHINE=173;
 	public static int PYRENEES=0;
 	public static int CANADA=300;
-	public static int MADAGASCAR=37;
-	public static int MARTINIQUE=312;
-	public static int TCHAD=23;
-	public static int DAKAR=7;
-	public static int GRECE=60;
-	public static int ALGERIE=15;
-	public static int TURQUIE=81;
+	public static int MADAGASCAR=89;
+	public static int MARTINIQUE=-48;
+	public static int TCHAD=79;
+	public static int DAKAR=68;
+	public static int GRECE=153;
+	public static int ALGERIE=79;
+	public static int TURQUIE=167;
 	public static int CUISSE_LA_MONTTE_FRONT=0;
-	public static int SRI_LANTA=145;
-	public static int MEUSE_FRONT=0;
-	public static int RUSSIE=141;
-	public static int MARNE=0;
-	public static int PAS_DE_CALAIS=0;
+	public static int SRI_LANTA=212;
+	public static int MEUSE_FRONT=48;
+	public static int RUSSIE=226;
+	public static int MARNE=32;
+	public static int PAS_DE_CALAIS=51;
 
 }
 
